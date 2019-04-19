3fdb6bf7dc1b07e65d37906ed1162789a8969e64
indes <wl@live.it>
2019-04-19 17:27:26 +0800

    添加用户资料缓存

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -9,27 +9,28 @@ use Classmate\Http\Requests;
 use Classmate\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
+use Redis;
 
 class HomeController extends Controller
 {
-    private $viewinfo=array('active'=>'home');
+    private $viewinfo = array('active' => 'home');
 
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($id=null)
+    public function index($id = null)
     {
 
-        if(!isset($id)) $id=session('user')->id;
-        $u=User::find($id);
-        $j=Journal::where('jAuthorId',$id)->orderBy('jPublishDate', 'desc')->paginate(6);
-        $j->count=Journal::where('jAuthorId',$id)->get()->count();
+        if (!isset($id)) $id = session('user')->id;
+        $u = User::get($id);
+        $j = Journal::where('jAuthorId', $id)->orderBy('jPublishDate', 'desc')->paginate(6);
+        $j->count = Journal::where('jAuthorId', $id)->get()->count();
 
-        $u->jcount=Journal::where('jAuthorId',$u->id)->get()->count();
-        $u->classcount=User::where('stuClassId',$u->stuClassId)->get()->count();
-        $this->viewinfo['title']=$u->userName.'的主页';
+        $u->jcount = Journal::where('jAuthorId', $u->id)->get()->count();
+        $u->classcount = User::where('stuClassId', $u->stuClassId)->get()->count();
+        $this->viewinfo['title'] = $u->userName . '的主页';
 
         return view('home/home')->withUser($u)->withJournal($j)->withInfo($this->viewinfo);
         //
@@ -37,37 +38,37 @@ class HomeController extends Controller
 
     public function login()
     {
-        $this->viewinfo['title']='登录';
+        $this->viewinfo['title'] = '登录';
         return view('home/login')->withInfo($this->viewinfo);
     }
 
     public function publish()
     {
-        $pbinfo=Input::get('pbinfo');
-        $j=new Journal();
-        $j->jData= $pbinfo;
-        $j->jAuthorId=session('user')->id;
-        $j->jPublishDate=date("Y-m-d H:i:s");
+        $pbinfo = Input::get('pbinfo');
+        $j = new Journal();
+        $j->jData = $pbinfo;
+        $j->jAuthorId = session('user')->id;
+        $j->jPublishDate = date("Y-m-d H:i:s");
         $j->save();
         return view('index.redirect')->withMsg("发布成功！")->withRdurl(url('/'));
     }
 
     public function del($id)
     {
-        $u=session('user');
-        $j=Journal::where('id',$id)->first();
-        if($j){
-            if($j->jAuthorId==$u->id){
-                if($j->delete()){
-                    return response()->json(['status'=>1]);
-                }else{
-                    return response()->json(['status'=>2]);
+        $u = session('user');
+        $j = Journal::where('id', $id)->first();
+        if ($j) {
+            if ($j->jAuthorId == $u->id) {
+                if ($j->delete()) {
+                    return response()->json(['status' => 1]);
+                } else {
+                    return response()->json(['status' => 2]);
                 }
-            }else{
-                return response()->json(['status'=>2]);
+            } else {
+                return response()->json(['status' => 2]);
             }
-        }else{
-            return response()->json(['status'=>2]);
+        } else {
+            return response()->json(['status' => 2]);
         }
     }
 
--- app/Http/Model/User.php
+++ app/Http/Model/User.php
@@ -9,10 +9,11 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Redis;
 
 class User extends Model implements AuthenticatableContract,
-                                    AuthorizableContract,
-                                    CanResetPasswordContract
+    AuthorizableContract,
+    CanResetPasswordContract
 {
     use Authenticatable, Authorizable, CanResetPassword;
 
@@ -36,4 +37,33 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
 //    protected $hidden = ['password', 'remember_token'];
+    public static function get($id)
+    {
+        $redis = new Redis();
+        $redis_user = Redis::hGetAll("user:profile:" . $id);
+        if ($redis_user) {
+            $user = new User();
+            foreach ($redis_user as $key=>$value){
+                $user->$key = $value;
+            }
+//            $user->name = $redis_user['userName'];
+            return $user;
+        } else {
+            $user = User::find($id);
+            Redis::hmset("user:profile:" . $id,
+                'id', $user->id,
+                'userName', $user->userName,
+                'stuName', $user->stuName,
+                'stuSex', $user->stuSex,
+                'stuImg', $user->stuImg,
+                'stuQQ', $user->stuQQ,
+                'stuPhone', $user->stuPhone,
+                'stuBio', $user->stuBio,
+                'stuClassId', $user->stuClassId,
+                'email', $user->email
+            );
+            return $user;
+        }
+
+    }
 }
